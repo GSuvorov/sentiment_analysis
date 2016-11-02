c593da24f8b1aaa35e214aace67ddea27790f920
Vladislav Shipovskoi <sgipovskoi@gmail.com>
2016-11-02 23:19:30 +0700

    Добавлен метод перекрестной проверки , вывод метрик precision recall f1, построение графика точночти обучения/тестирования

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from scipy.sparse import spmatrix, coo_matrix
 from sklearn.base import BaseEstimator
 from sklearn.linear_model.base import LinearClassifierMixin, SparseCoefMixin
 from sklearn.svm import LinearSVC
+import sklearn.metrics as metrics
 from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
 
@@ -121,13 +122,72 @@ def load_data():
     return text, sentiment
 
 
+def crossvalidation(x, y, vectorizer, classifier):
+    X_folds = np.array_split(x, 10)
+    y_folds = np.array_split(y, 10)
+    scorestrain = list()
+    scorestest = list()
+    for k in range(10):
+        print(k)
+        X_train = list(X_folds)
+        X_test = X_train.pop(k)
+        X_train = np.concatenate(X_train)
+        y_train = list(y_folds)
+        y_test = y_train.pop(k)
+        y_train = np.concatenate(y_train)
+        X_train = vectorizer.fit_transform(X_train)
+        X_test = vectorizer.transform(X_test)
+        scorestrain.append(classifier.fit(X_train, y_train).score(X_train, y_train))
+        scorestest.append(classifier.fit(X_train, y_train).score(X_test, y_test))
+        y_predicted = classifier.predict(X_test)
+        print("*****")
+        print("Отчет классификации - %s" % classifier)
+        print(metrics.classification_report(y_test, y_predicted))
+    return scorestrain, scorestest
+
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+def learning_curves(title, x, y, estimator):
+    cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+    plot_learning_curve(estimator, title, x, y, (0.1, 1.), cv=cv, n_jobs=4)
+    plt.show()
+
+
 def main():
     token_pattern = r'\w+|[%s]' % string.punctuation
     vectorizer = CountVectorizer(ngram_range=(1, 1), token_pattern=token_pattern, binary=False)
     text, sentiment = load_data()
     X = vectorizer.fit_transform(text)
     classifier = LinearSVC()
-    #scorestrain, scorestest = crossvalidation(text, sentiment, vectorizer, classifier)
+    scorestrain, scorestest = crossvalidation(text, sentiment, vectorizer, classifier)
     # classifier=SVC(gamma=0.001)
     # learning_curves("Learning Curves (SVM, Linear kernel, $\gamma=0.001$",X,sentiment,classifier)
 
