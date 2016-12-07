c68e2a2671fadbedfb332625fc79b6f5e9a32e31
Vladislav Shipovskoi <sgipovskoi@gmail.com>
2016-12-08 01:21:21 +0700

    Кириллические подписи к графикам

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from sklearn.linear_model.base import LinearClassifierMixin, SparseCoefMixin
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -16,6 +17,7 @@ import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn import metrics
 from pyaspeller import Word
+from matplotlib import rc
 import pandas as pd
 import numpy as np
 import pymorphy2
@@ -268,12 +270,16 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, tr
     n_jobs : integer, optional
         Number of jobs to run in parallel (default 1).
     """
+    font = {'family': 'Droid Sans',
+            'weight': 'normal',
+            'size': 14}
+    plt.rc('font', **font)
     plt.figure()
     plt.title(title)
     if ylim is not None:
         plt.ylim(*ylim)
-    plt.xlabel("Training examples")
-    plt.ylabel("Score")
+    plt.xlabel(u"Объем тренировочной выборки")
+    plt.ylabel(u"Точность")
     train_sizes, train_scores, test_scores = learning_curve(
         estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
     train_scores_mean = np.mean(train_scores, axis=1)
@@ -288,9 +294,9 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, tr
     plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
                      test_scores_mean + test_scores_std, alpha=0.1, color="g")
     plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
+             label=u"Точность обучения")
     plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
+             label=u"Точность тестирования")
 
     plt.legend(loc="best")
     print('train scores')
